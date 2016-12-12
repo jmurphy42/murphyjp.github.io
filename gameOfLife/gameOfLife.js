7cac6dcd0871180fc7861842dc58c34225630b6d
Jason Murphy <murphyjp@msoe.edu>
2016-12-12 17:22:40 -0600

    Added another item: Conway's Game of Life

--- gameOfLife/gameOfLife.js
+++ gameOfLife/gameOfLife.js
@@ -7,21 +7,27 @@ var status = 0;
 
 var gridData = [];
 var newGridData = [];
+var history1 = [],
+    history2 = [],
+    history3 = [],
+    history4 = [],
+    history5 = [];
 
 var worldSave = [];
 
 var delay = 100;
-var changedCells = [0,0,0,0,0];
+var changedCells = 0;
+var generations = 0;
 var looper;
 
-function run(){
+function run() {
 
     clearInterval(looper);
     looper = setInterval(run, delay);
 
-    for (var dx = 0; dx<80; dx++){      //for all x rows
-        for (var dy = 0; dy<80; dy++) {  //for all y cells in x row
-            var cellSum = getNeighbors(dx,dy);
+    for (var dx = 0; dx < 80; dx++) {      //for all x rows
+        for (var dy = 0; dy < 80; dy++) {  //for all y cells in x row
+            var cellSum = getNeighbors(dx, dy);
             switch (cellSum) {
                 case 0:
                 case 1:
@@ -46,78 +52,81 @@ function run(){
 
     paintNew();
     copyNewOntoOld();
-    reportLiveCells();
+    generations++;
+    reportStats();
+    recordHistory();
     checkActivity();
 
-    function reportLiveCells(){
+    function reportStats() {
         document.getElementById("report").innerHTML =
-            "Live Cells: " + getGridCount(gridData) + " &nbsp; &nbsp; Active Cells: " + changedCells[0];
+            "<h3 class='stat'>" + "Generations: " + generations + "</h3>" +
+            "<h3 class='stat'>" + " Cells: " + getGridCount(gridData) + "</h3>" +
+            "<h3 class='stat'>" + "Active Cells: " + changedCells + "</h3>";
     }
 
-    function copyNewOntoOld(){
+    function copyNewOntoOld() {
         //copy newGridData onto gridData
-        shiftActivity();
-        changedCells[0] = 0;
-        for (var ddx = 0; ddx<80; ddx++) {      //for all x rows
+        changedCells = 0;
+        for (var ddx = 0; ddx < 80; ddx++) {      //for all x rows
             for (var ddy = 0; ddy < 80; ddy++) {  //for all y cells in x row
-                if (gridData[ddx][ddy] != newGridData[ddx][ddy]){
-                    changedCells[0]++;
+                if (gridData[ddx][ddy] != newGridData[ddx][ddy]) {
+                    changedCells++;
                 }
                 gridData[ddx][ddy] = newGridData[ddx][ddy];
             }
         }
     }
 
-    function getNeighbors(x,y){
+    function getNeighbors(x, y) {
         var ul, u, ur, l, r, dl, d, dr;
-        if ((x-1)<0 || (y-1)<0){
+        if ((x - 1) < 0 || (y - 1) < 0) {
             ul = 0;
-        }else{
+        } else {
             ul = gridData[(x - 1)][(y - 1)];
         }
-        if ((y-1)<0){
+        if ((y - 1) < 0) {
             u = 0;
-        }else{
+        } else {
             u = gridData[(x)][(y - 1)];
         }
-        if ((x+1)>79 || (y-1)<0){
+        if ((x + 1) > 79 || (y - 1) < 0) {
             ur = 0;
-        }else{
-            ur = gridData[(x+1)][(y - 1)];
+        } else {
+            ur = gridData[(x + 1)][(y - 1)];
         }
-        if ((x-1)<0){
+        if ((x - 1) < 0) {
             l = 0;
-        }else{
-            l = gridData[(x-1)][(y)];
+        } else {
+            l = gridData[(x - 1)][(y)];
         }
-        if ((x+1)>79){
+        if ((x + 1) > 79) {
             r = 0;
-        }else{
-            r = gridData[(x+1)][(y)];
+        } else {
+            r = gridData[(x + 1)][(y)];
         }
-        if ((x-1)<0 || (y+1)>79){
+        if ((x - 1) < 0 || (y + 1) > 79) {
             dl = 0;
-        }else{
+        } else {
             dl = gridData[(x - 1)][(y + 1)];
         }
-        if ((y+1)>79){
+        if ((y + 1) > 79) {
             d = 0;
-        }else{
+        } else {
             d = gridData[(x)][(y + 1)];
         }
-        if ((x+1)>79 || (y+1)>79){
+        if ((x + 1) > 79 || (y + 1) > 79) {
             dr = 0;
-        }else{
-            dr = gridData[(x+1)][(y+1)];
+        } else {
+            dr = gridData[(x + 1)][(y + 1)];
         }
-        return ul+u+ur+l+r+dl+d+dr;
+        return ul + u + ur + l + r + dl + d + dr;
     }
 
-    function paintNew(){
+    function paintNew() {
         var canvas = document.getElementById('canvas');
         var context = canvas.getContext('2d');
-        for (var dx = 0; dx<80; dx++){      //for all x rows
-            for (var dy = 0; dy<80; dy++) {  //for all y cells in x row
+        for (var dx = 0; dx < 80; dx++) {      //for all x rows
+            for (var dy = 0; dy < 80; dy++) {  //for all y cells in x row
                 fillSquare(context, dx, dy, newGridData[dx][dy]);
             }
         }
@@ -127,35 +136,50 @@ function run(){
         var cSelected = "#222222";
         var cUnselected = "#dddddd";
 
-        var x = dx*8+1; //get x pixel
-        var y = dy*8+1; //get y pixel
+        var x = dx * 8 + 1; //get x pixel
+        var y = dy * 8 + 1; //get y pixel
 
-        if (type==1) {
+        if (type == 1) {
             context.fillStyle = cSelected;
             context.fillRect(x, y, 7, 7);
-        }else if(type==0){
+        } else if (type == 0) {
             context.fillStyle = cUnselected;
             context.fillRect(x, y, 7, 7);
         }
     }
 
-    function shiftActivity(){
-        changedCells.unshift(0);
-    }
+    function checkActivity() {
+        var duplicate = isDuplicate(history1,history2) ||
+            isDuplicate(history1,history3) ||
+            isDuplicate(history1,history4) ||
+            isDuplicate(history1,history5) ||
+            isDuplicate(history2,history3) ||
+            isDuplicate(history2,history4) ||
+            isDuplicate(history2,history5) ||
+            isDuplicate(history3,history4) ||
+            isDuplicate(history3,history5) ||
+            isDuplicate(history4,history5);
 
-    function checkActivity(){
-        var sum = 0;
-        for (var i = 0; i<5; i++){
-            sum += changedCells[i];
+        function isDuplicate(grid1, grid2) {
+            for (var x = 0; x < 80; x++) {      //for all x rows
+                for (var y = 0; y < 80; y++) {  //for all y cells in x row
+                    if (grid2[x][y] != grid1[x][y]){
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
-        if (sum <= 0){
+
+        if (duplicate){
             stopLooper();
         }
     }
 }
 
-function runLooper(){
-    if (status==0) {
+function runLooper() {
+    if (status == 0) {
+        generations = 0;
         var frame = document.getElementById("gameFrame");
         frame.style.borderColor = "#9a2517";
         frame = document.getElementById("outerFrame");
@@ -166,8 +190,8 @@ function runLooper(){
     }
 }
 
-function stopLooper(){
-    if (status==1) {
+function stopLooper() {
+    if (status == 1) {
         var frame = document.getElementById("gameFrame");
         frame.style.borderColor = "#252735";
         frame = document.getElementById("outerFrame");
@@ -177,28 +201,36 @@ function stopLooper(){
     }
 }
 
-function saveWorld(){
-    for (var x=0; x<80; x++){
-        for (var y=0; y<80; y++){
+function saveWorld() {
+    stopLooper();
+    for (var x = 0; x < 80; x++) {
+        for (var y = 0; y < 80; y++) {
             worldSave[x][y] = gridData[x][y];
         }
     }
 }
 
-function restoreWorld(){
-    for (var x=0; x<80; x++){
-        for (var y=0; y<80; y++){
+function restoreWorld() {
+    stopLooper();
+
+    for (var x = 0; x < 80; x++) {
+        for (var y = 0; y < 80; y++) {
             gridData[x][y] = worldSave[x][y];
         }
     }
 
     paintNew();
+    fillGrid(history1, 1);
+    fillGrid(history2, 2);
+    fillGrid(history3, 3);
+    fillGrid(history4, 4);
+    fillGrid(history5, 5);
 
-    function paintNew(){
+    function paintNew() {
         var canvas = document.getElementById('canvas');
         var context = canvas.getContext('2d');
-        for (var dx = 0; dx<80; dx++){      //for all x rows
-            for (var dy = 0; dy<80; dy++) {  //for all y cells in x row
+        for (var dx = 0; dx < 80; dx++) {      //for all x rows
+            for (var dy = 0; dy < 80; dy++) {  //for all y cells in x row
                 fillSquare(context, dx, dy, gridData[dx][dy]);
             }
         }
@@ -208,22 +240,48 @@ function restoreWorld(){
         var cSelected = "#222222";
         var cUnselected = "#dddddd";
 
-        var x = dx*8+1; //get x pixel
-        var y = dy*8+1; //get y pixel
+        var x = dx * 8 + 1; //get x pixel
+        var y = dy * 8 + 1; //get y pixel
 
-        if (type==1) {
+        if (type == 1) {
             context.fillStyle = cSelected;
             context.fillRect(x, y, 7, 7);
-        }else if(type==0){
+        } else if (type == 0) {
             context.fillStyle = cUnselected;
             context.fillRect(x, y, 7, 7);
         }
     }
 }
 
-function getGridCount(grid){
+function fillGrid(grid, value){
+    for (var x = 0; x < 80; x++) {
+        for (var y = 0; y < 80; y++) {
+            grid[x][y] = value;
+        }
+    }
+}
+
+function recordHistory() {
+    //shift histories
+    copy(history4,history5);
+    copy(history3,history4);
+    copy(history2,history3);
+    copy(history1,history2);
+    copy(newGridData,history1);
+
+    //copy grid onto other grid
+    function copy(copyGrid, pasteToGrid) {
+        for (var x = 0; x < 80; x++) {      //for all x rows
+            for (var y = 0; y < 80; y++) {  //for all y cells in x row
+                pasteToGrid[x][y] = copyGrid[x][y];
+            }
+        }
+    }
+}
+
+function getGridCount(grid) {
     var sum = 0;
-    for (var x = 0; x<80; x++) {      //for all x rows
+    for (var x = 0; x < 80; x++) {      //for all x rows
         for (var y = 0; y < 80; y++) {  //for all y cells in x row
             sum += grid[x][y];
         }
@@ -231,12 +289,12 @@ function getGridCount(grid){
     return sum;
 }
 
-function verifySpeed(){
+function verifySpeed() {
     var speedInput = document.getElementById("speed");
     var speedValue = parseInt(speedInput.value);
-    if (isNaN(speedValue) || speedValue<20 || speedValue>1000){
-        speedInput.value = delay+"";
-    }else{
+    if (isNaN(speedValue) || speedValue < 20 || speedValue > 1000) {
+        speedInput.value = delay + "";
+    } else {
         delay = speedValue;
     }
 }
@@ -253,20 +311,35 @@ function init() {
     addMouseClick();
     addMouseOut();
 
-    function setUpGridData(){
+    function setUpGridData() {
         //Create a gridData 80x80 array of arrays to hold cell 1/0 status.
-        for (var x = 0; x<80; x++) {
-            var yArray = []; //create a new yArray.
-            var yArray2 = []; //create a new yArray2.
-            var yArray3 = []; //create a new yArray3.
+        for (var x = 0; x < 80; x++) {
+            var yArray1 = []; //create a new yArray.
+            var yArray2 = []; //create a new yArray.
+            var yArray3 = []; //create a new yArray.
+            var yArray4 = []; //create a new yArray.
+            var yArray5 = []; //create a new yArray.
+            var yArray6 = []; //create a new yArray.
+            var yArray7 = []; //create a new yArray.
+            var yArray8 = []; //create a new yArray.
             for (var y = 0; y < 80; y++) {
-                yArray.push(0); //give a yArray 80 cells.
-                yArray2.push(0); //give a yArray2 80 cells.
-                yArray3.push(0); //give a yArray3 80 cells.
+                yArray1.push(0); //give a yArray 80 cells.
+                yArray2.push(0); //give a yArray 80 cells.
+                yArray3.push(0); //give a yArray 80 cells.
+                yArray4.push(1); //give a yArray 80 cells.
+                yArray5.push(2); //give a yArray 80 cells.
+                yArray6.push(3); //give a yArray 80 cells.
+                yArray7.push(4); //give a yArray 80 cells.
+                yArray8.push(5); //give a yArray 80 cells.
             }
-            gridData.push(yArray); //add the new yArray to gridData 80 times.
-            newGridData.push(yArray2); //add the new yArray2 to newGridData 80 times.
-            worldSave.push(yArray3); //add the new yArray3 to worldSave 80 times.
+            gridData.push(yArray1); //add the new yArray to grid 80 times.
+            newGridData.push(yArray2); //add the new yArray to grid 80 times.
+            worldSave.push(yArray3); //add the new yArray to grid 80 times.
+            history1.push(yArray4); //add the new yArray to grid 80 times.
+            history2.push(yArray5); //add the new yArray to grid 80 times.
+            history3.push(yArray6); //add the new yArray to grid 80 times.
+            history4.push(yArray7); //add the new yArray to grid 80 times.
+            history5.push(yArray8); //add the new yArray to grid 80 times.
         }
         //the structure is: gridData[x][y] = cell (x,y)
         //                  gridData[0][0] = top left cell (0,0)
@@ -312,41 +385,41 @@ function init() {
         var cUnselected = "#dddddd";
         var cUnselectedHover = "#8b8b8b";
 
-        if (type=="click") {
-            if (hex == cSelected || hex == cSelectedHover){
+        if (type == "click") {
+            if (hex == cSelected || hex == cSelectedHover) {
                 context.fillStyle = cUnselected;
                 context.fillRect(px, py, 7, 7);
-                gridData[cx][cy]=0;
+                gridData[cx][cy] = 0;
                 mouseMode = "unselecting";
-            }else{
+            } else {
                 context.fillStyle = cSelected;
                 context.fillRect(px, py, 7, 7);
-                gridData[cx][cy]=1;
+                gridData[cx][cy] = 1;
                 mouseMode = "selecting";
             }
-        }else if (type=="clickmove") {
-            if (mouseMode == "unselecting"){
+        } else if (type == "clickmove") {
+            if (mouseMode == "unselecting") {
                 context.fillStyle = cUnselected;
                 context.fillRect(px, py, 7, 7);
-                gridData[cx][cy]=0;
-            }else if (mouseMode == "selecting"){
+                gridData[cx][cy] = 0;
+            } else if (mouseMode == "selecting") {
                 context.fillStyle = cSelected;
                 context.fillRect(px, py, 7, 7);
-                gridData[cx][cy]=1;
+                gridData[cx][cy] = 1;
             }
-        }else if (type=="hover"){
+        } else if (type == "hover") {
             if (hex == cSelected || hex == cSelectedHover) {
                 context.fillStyle = cSelectedHover;
                 context.fillRect(px, py, 7, 7);
-            }else{
+            } else {
                 context.fillStyle = cUnselectedHover;
                 context.fillRect(px, py, 7, 7);
             }
-        }else if (type=="unhover"){
+        } else if (type == "unhover") {
             if (hex == cSelectedHover || hex == cSelected) {
                 context.fillStyle = cSelected;
                 context.fillRect(px, py, 7, 7);
-            }else{
+            } else {
                 context.fillStyle = cUnselected;
                 context.fillRect(px, py, 7, 7);
             }
@@ -354,15 +427,17 @@ function init() {
 
     }
 
-    function addMouseOut(){
+    function addMouseOut() {
         canvas.addEventListener('mouseout', function (event) {
-            if (status==0) {
+            if (status == 0) {
+                document.getElementById("cellReport").innerHTML =
+                    "";
                 canvas.style.cursor = "default";
                 fillSquare(context, previousSquare.x, previousSquare.y, 0, 0, "unhover");
                 if (event.which == 0) { //mouse button not down
                     mouseMode = "none";
                 }
-            }else{
+            } else {
                 canvas.style.cursor = "default";
             }
         }, false);
@@ -370,7 +445,7 @@ function init() {
 
     function addMouseClick() {
         canvas.addEventListener('click', function (event) {
-            if (status==0) {
+            if (status == 0) {
                 canvas.style.cursor = "default";
                 var mousePos = getSquare(canvas, event);
                 var cellPos = getCell(canvas, event);
@@ -384,7 +459,7 @@ function init() {
                 }
                 currentSquare = mousePos;
                 fillSquare(context, mousePos.x, mousePos.y, xPos, yPos, "click");
-            }else{
+            } else {
                 canvas.style.cursor = "crosshair";
             }
         }, false);
@@ -392,7 +467,7 @@ function init() {
 
     function addMouseOver() {
         canvas.addEventListener('mousemove', function (event) {
-            if (status==0) {
+            if (status == 0) {
                 canvas.style.cursor = "default";
                 var mousePos = getSquare(canvas, event);
                 var cellPos = getCell(canvas, event);
@@ -404,7 +479,7 @@ function init() {
                 if (xPos == -1) {
                     xPos = 0;
                 }
-                document.getElementById("report").innerHTML =
+                document.getElementById("cellReport").innerHTML =
                     "Cell: (" + (xPos + 1) + "," + (yPos + 1) + ")";
                 if (event.which == 1) { //mouse button down
                     currentSquare = mousePos;
@@ -417,7 +492,7 @@ function init() {
                     fillSquare(context, previousSquare.x, previousSquare.y, xPos, yPos, "unhover");
                     fillSquare(context, mousePos.x, mousePos.y, xPos, yPos, "hover");
                 }
-            }else{
+            } else {
                 canvas.style.cursor = "crosshair";
             }
         }, false);
